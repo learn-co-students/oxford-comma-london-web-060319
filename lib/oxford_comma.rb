a2c971b672f5a11e27e67ae262ffe8904aa45eda
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-15 07:18:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  
+if array.length==1 
+  return "#{array[0]}"
+  
+elsif array.length==2 
+    return "#{array[0]} and #{array[1]}"
 
+else
+ last=array.pop
+array.join(", ")<< ", and #{last}" 
+  end
 end
+
+
+ 

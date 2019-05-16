c3b984486c2f9839b8555f104fea6bb5813d3870
github username <github email address>
2019-05-16 23:36:39 +0100

    Completed task

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  else
+    array << "and #{array.pop}"
+    if array.length == 2
+      array.join(" ")
+    else
+      array.join(", ")
+    end
+  end
 end

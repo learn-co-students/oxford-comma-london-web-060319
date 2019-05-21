5df1ab6bff10ef380e1de30fb1e27ffce72c0a1d
Federica Macchi <federica.macchi3@gmail.com>
2019-05-21 18:55:50 +0100

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        new_string = array.join
+    elsif array.length == 2
+        new_string = array.join(" and ")
+    else #array.length == 3
+        letters = array[(array.length - 1)].length
+        letters += 1
+        new_string = array.join(", ")
+        new_string = new_string.insert(-letters, "and ")
+    end
+    puts new_string
+    return new_string
 end
